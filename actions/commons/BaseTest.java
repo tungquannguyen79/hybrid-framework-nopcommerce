4a12a34317344168155333a3bba833c56ffe9882
QuanNT <tungquannguyen79@gmail.com>
2024-02-23 23:41:50 +0700

    Update Multiple Browser and sample for Page Factory

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,24 @@
 package commons;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class BaseTest {
+	String projectPath = System.getProperty("user.dir");
+	private WebDriver driver;
 // Chứa những hàm dùng chung cho tầng Testcase
+	protected WebDriver getBrowserDriver(String browserName) {
+		if (browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDriver\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDriver\\chromedriver.exe");
+			driver = new ChromeDriver();
+		} else {
+			throw new RuntimeException("Browser name invalid");
+		}
+		return driver;
+
+	}
 }
